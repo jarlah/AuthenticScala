2785d06aaf5a9173810bc426623ed0c0202ae9bf
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-08-15 21:45:16 +0200

    case class all the things

--- src/main/scala/com/github/jarlah/authenticscala/basic/BasicAuthenticatorConfiguration.scala
+++ src/main/scala/com/github/jarlah/authenticscala/basic/BasicAuthenticatorConfiguration.scala
@@ -2,16 +2,12 @@ package com.github.jarlah.authenticscala.basic
 import com.github.jarlah.authenticscala.AuthenticatorConfiguration
 import com.typesafe.config.Config
 
-abstract class BasicAuthenticatorConfiguration()
-    extends AuthenticatorConfiguration {
-  val realm: String
-}
+case class BasicAuthenticatorConfiguration(realm: String)
+    extends AuthenticatorConfiguration
 
 object BasicAuthenticatorConfiguration {
   def apply(config: Config): BasicAuthenticatorConfiguration = {
     val basicConfig = config.getConfig("authentic.basic")
-    new BasicAuthenticatorConfiguration() {
-      override val realm: String = basicConfig.getString("realm")
-    }
+    BasicAuthenticatorConfiguration(basicConfig.getString("realm"))
   }
 }
--- src/main/scala/com/github/jarlah/authenticscala/digest/DigestAuthenticatorConfiguration.scala
+++ src/main/scala/com/github/jarlah/authenticscala/digest/DigestAuthenticatorConfiguration.scala
@@ -3,21 +3,20 @@ package com.github.jarlah.authenticscala.digest
 import com.github.jarlah.authenticscala.AuthenticatorConfiguration
 import com.typesafe.config.Config
 
-abstract class DigestAuthenticatorConfiguration()
-    extends AuthenticatorConfiguration {
-  val encoder: PrivateHashEncoder
-  val nonceTimeout: Long
-  val realm: String
-}
+case class DigestAuthenticatorConfiguration(
+    encoder: PrivateHashEncoder,
+    nonceTimeout: Long,
+    realm: String
+) extends AuthenticatorConfiguration
 
 object DigestAuthenticatorConfiguration {
   def apply(config: Config): DigestAuthenticatorConfiguration = {
-    val conf = config.getConfig("authentic.digest")
-    new DigestAuthenticatorConfiguration() {
-      override val nonceTimeout: Long = conf.getLong("nonce.timeout")
-      override val realm: String      = conf.getString("realm")
-      private val noncePrivateKey     = conf.getString("nonce.privateKey")
-      override val encoder            = PrivateHashEncoder(noncePrivateKey)
-    }
+    val conf       = config.getConfig("authentic.digest")
+    val privateKey = conf.getString("nonce.privateKey")
+    DigestAuthenticatorConfiguration(
+      PrivateHashEncoder(privateKey),
+      conf.getLong("nonce.timeout"),
+      conf.getString("realm")
+    )
   }
 }
