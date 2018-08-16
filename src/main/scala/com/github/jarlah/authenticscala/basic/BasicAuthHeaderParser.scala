ca5cbd4eba581a085be1b8fe5c1b90a5567c6fb2
Jarl André Hübenthal <jarl.andre@gmail.com>
2018-08-16 17:02:03 +0200

    replacing 6 lines with 2

--- src/main/scala/com/github/jarlah/authenticscala/basic/BasicAuthHeaderParser.scala
+++ src/main/scala/com/github/jarlah/authenticscala/basic/BasicAuthHeaderParser.scala
@@ -5,20 +5,17 @@ object BasicAuthHeaderParser {
   private val headerPrefix = "Basic "
 
   def extractBasicHeader(authHeader: String): Option[BasicHeader] = {
-    getHeaderValue(authHeader).flatMap(headerValue => {
+    getHeaderValue(authHeader).map(headerValue => {
       val decoded  = Base64Utils.decode(headerValue)
       val userPass = decoded.split(":")
-      if (userPass.size == 2) {
-        Some(BasicHeader(userPass(0), userPass(1)))
-      } else {
-        None
-      }
+      BasicHeader(userPass(0), userPass(1))
     })
   }
   private[this] def getHeaderValue(authHeader: String): Option[String] = {
     if (authHeader == null
         || authHeader.isEmpty
-        || !authHeader.startsWith(headerPrefix)) {
+        || !authHeader.startsWith(headerPrefix)
+        || !authHeader.contains(":")) {
       return None
     }
     Some(authHeader.substring(headerPrefix.length))
